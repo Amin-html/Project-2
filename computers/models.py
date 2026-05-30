8c53ee23970953aad155c8df6a9249d2e61138dd
Amin911 <adilov.amin.rx7.a86@gmail.com>
2026-05-30 21:27:32 +0600

    feat: добавил модели Computer и Booking

--- computers/models.py
+++ computers/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+class Computer(models.Model):
+    ZONE_CHOICES = [
+        ('standard', 'Стандартная зона'),
+        ('vip', 'VIP зона'),
+        ('gaming', 'Игровая зона'),
+    ]
+    name = models.CharField(max_length=100)
+    zone = models.CharField(max_length=25, choices=ZONE_CHOICES)
+    cpu = models.CharField(max_length=100)
+    gpu = models.CharField(max_length=100)
+    ram = models.IntegerField()
+    price_per_hour = models.IntegerField()
+    is_available = models.BooleanField(default=True)
+    image = models.ImageField(upload_to='computers/', blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.name} ({self.get_zone_display()})'
 # Create your models here.
